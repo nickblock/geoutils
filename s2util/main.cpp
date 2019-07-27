4699128ae386eef85117af45f3e0c9114e2b3b03
Unknown <nickblock@hotmail.com>
2019-07-27 09:08:23 +0100

    s2 needed pthread to work

--- s2util/main.cpp
+++ s2util/main.cpp
@@ -2,6 +2,9 @@
 #include "tinyformat.h"
 #include <stdint.h>
 #include <regex>
+#include "s2/s2cell.h"
+#include "s2/s2latlng.h"
+#include "s2/s2cell_id.h"
 
 using std::string;
 using std::cout;
@@ -13,7 +16,7 @@ int main(int argi, char** argv)
   args::ArgumentParser  parser("s2util. Return coordinates of teh center of an s2 cell.");
   args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
 
-  args::Positional<string>  inputS2Cell(parser, "0x0", "Enter a string containing an 8 digit hex number");
+  args::Positional<string>  inputS2Cell(parser, "0x0", "Enter a string containing an 8 digit hex number representing a valid s2 cell id");
 
   try
   {
@@ -31,26 +34,33 @@ int main(int argi, char** argv)
       std::exit(1); 
   }
 
-  uint64_t cellId;
-
   try {
     
     std::smatch res; 
-    std::regex reg(".*([0-9a-f]{8}).*");
+    std::regex reg(".*([0-9a-f]{16}).*");
     std::regex_match (args::get(inputS2Cell), res, reg);
-    if (res.size() > 1)
-      cout << "string object matched " << res[1] << endl;
-    else 
-      cerr << "failed to match" << endl;
+    if (res.size() <= 1) {
+      cerr << "failed to find cell id in " << endl;
+      std::exit(1);
+    }
 
-    cellId = std::stoul(res[1], nullptr, 16);
+    uint64_t number = std::stoul(res[1], nullptr, 16);
+
+    const S2CellId cellId(number);
+
+    if(cellId.is_valid()) {
+      const S2LatLng cellCenter = cellId.ToLatLng();
+        
+      cout << cellCenter.lat() << ", " << cellCenter.lng() << endl;
+    }
+    else {
+      std::cerr << "Invalid cell Id (" << number << ")" << endl;
+    }
   }
-  catch (std::invalid_argument ex) {
-    std::cerr << "Couldn't decode input as hex number." << endl;
+  catch (const std::exception& ex) {
+    std::cerr << "Couldn't decode input as hex number. " << ex.what() << endl;
     std::exit(1);
   }
-
-  cout << cellId << endl;
 
   return 0;
 }
