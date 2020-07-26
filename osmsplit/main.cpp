c5300f8739b553d5c1be5d125ed648fcf69e8299
nick <nick@hotmail.com>
2020-07-26 16:16:05 +0100

    config files jhave json extension

--- osmsplit/main.cpp
+++ osmsplit/main.cpp
@@ -23,135 +23,152 @@
 #include "osmsplitconfig.h"
 #include "mapsplit.h"
 
+using std::cerr;
 using std::cout;
 using std::endl;
-using std::cerr;
+using std::mutex;
+using std::ofstream;
+using std::set;
 using std::string;
 using std::thread;
-using std::mutex;
 using std::vector;
-using std::set;
-using std::ofstream;
 
 using GeoUtils::MapHandler;
-using GeoUtils::OSMSplitWriter;
-using GeoUtils::OSMSplitConfigPtr;
-using GeoUtils::OSMSplitConfig;
 using GeoUtils::OSMConfigList;
+using GeoUtils::OSMSplitConfig;
+using GeoUtils::OSMSplitConfigPtr;
+using GeoUtils::OSMSplitWriter;
 
 namespace fs = std::experimental::filesystem;
 
-
 using IdSet = set<osmium::unsigned_object_id_type>;
 
-
-std::clock_t startTime;  
+std::clock_t startTime;
 uint64_t numLocs = 0;
 
-string constructOutPrefix(const string& inputFile) {
+const std::string configFileExt = "_conf.json";
+
+string constructOutPrefix(const string &inputFile)
+{
 
   string fileName = inputFile;
-  
+
   int lastSlash = fileName.find_last_of("/");
-  if(lastSlash != string::npos)
-    fileName = fileName.substr(lastSlash+1, fileName.size());
-  
+  if (lastSlash != string::npos)
+    fileName = fileName.substr(lastSlash + 1, fileName.size());
+
   int spotPos = fileName.find_first_of(".");
-  if(spotPos != string::npos)
+  if (spotPos != string::npos)
     fileName = fileName.substr(0, spotPos);
 
   return fileName;
 }
 
-string addSlash(const std::string dirName) {
+string addSlash(const std::string dirName)
+{
 
   string outDir = dirName;
-  if(outDir[outDir.size()-1] != '/') {
+  if (outDir[outDir.size() - 1] != '/')
+  {
     outDir += string("/");
   }
   return outDir;
 }
 
-string fileNameFromPath(const string& path) {
+string fileNameFromPath(const string &path)
+{
 
   string nameOnly = path;
   int slashPos = path.find_last_of("/");
-  if(slashPos != string::npos) {
-    nameOnly = nameOnly.substr(slashPos+1, nameOnly.size());
+  if (slashPos != string::npos)
+  {
+    nameOnly = nameOnly.substr(slashPos + 1, nameOnly.size());
   }
 
   return nameOnly;
 }
 
-string getDirFromPath(const string& path) {
+string getDirFromPath(const string &path)
+{
 
   string pathOnly = path;
   int slashPos = path.find_last_of("/");
-  if(slashPos != string::npos) {
-    pathOnly = pathOnly.substr(0, slashPos+1);
+  if (slashPos != string::npos)
+  {
+    pathOnly = pathOnly.substr(0, slashPos + 1);
   }
   return pathOnly;
 }
 
-int getMemUse() {
+int getMemUse()
+{
   osmium::MemoryUsage mem;
   return mem.current();
 }
 
-int getElapsedTime() {
+int getElapsedTime()
+{
 
   double now = std::clock();
-  return (now - startTime)/(double)CLOCKS_PER_SEC;
+  return (now - startTime) / (double)CLOCKS_PER_SEC;
 }
 
-void printMemTimeUpdate() {
-  cout << "\r" << "elapsed " << getElapsedTime() << " mem : " << getMemUse() << endl;
+void printMemTimeUpdate()
+{
+  cout << "\r"
+       << "elapsed " << getElapsedTime() << " mem : " << getMemUse() << endl;
 }
 
-void writeConfigFile(std::string configFileName, OSMSplitConfigPtr config) {
+void writeConfigFile(std::string configFileName, OSMSplitConfigPtr config)
+{
 
   ofstream ofs(configFileName);
   rapidjson::OStreamWrapper osw(ofs);
   rapidjson::PrettyWriter<rapidjson::OStreamWrapper> writer(osw);
   rapidjson::Document configDoc;
-  rapidjson::Document::AllocatorType& a = configDoc.GetAllocator();
+  rapidjson::Document::AllocatorType &a = configDoc.GetAllocator();
   configDoc.SetObject().AddMember("osmsplit", config->toJson(a), a);
   configDoc.Accept(writer);
 }
 
-void processOSMFile(std::string inputFileName, std::string outDir, OSMSplitConfigPtr& config, SplitOptions options)
+void processOSMFile(std::string inputFileName, std::string outDir, OSMSplitConfigPtr &config, SplitOptions options)
 {
   string outFileNamePrefix = constructOutPrefix(inputFileName);
 
-  if(options.updateOnly) {
+  if (options.updateOnly)
+  {
     string potentialOutFile = outDir + outFileNamePrefix;
-    for(int i=0; i<options.depthLevels; i++) 
+    for (int i = 0; i < options.depthLevels; i++)
       potentialOutFile += string("0");
 
     potentialOutFile += OSMSplitConfig::suffix();
 
-    if(fs::exists(potentialOutFile)) {
+    if (fs::exists(potentialOutFile))
+    {
       auto newTime = fs::last_write_time(potentialOutFile);
       auto inputTime = fs::last_write_time(inputFileName);
 
-      if(newTime > inputTime) {
+      if (newTime > inputTime)
+      {
         cout << "Skipping existing outputs like '" << potentialOutFile << "'" << endl;
         return;
       }
     }
   }
 
-  osmium::io::File inputOsFile{inputFileName}; 
-  osmium::io::Reader reader{inputOsFile, osmium::osm_entity_bits::node | osmium::osm_entity_bits::way}; 
+  osmium::io::File inputOsFile{inputFileName};
+  osmium::io::Reader reader{inputOsFile, osmium::osm_entity_bits::node | osmium::osm_entity_bits::way};
   cout << "Procssing File " << inputFileName << endl;
 
-  if(config == nullptr) {
+  if (config == nullptr)
+  {
     config = std::make_shared<OSMSplitConfig>(reader.header().box(), outFileNamePrefix);
   }
-  
+
   NodeLocatorMap nodeLocatorStore;
 
-  if(!reader.header().box()) {
+  if (!reader.header().box())
+  {
     cout << "No extents in header" << endl;
     return;
   }
@@ -161,7 +178,8 @@ void processOSMFile(std::string inputFileName, std::string outDir, OSMSplitConfi
 
   cout << "Read Locations" << endl;
   printMemTimeUpdate();
-  cout << "nodes " << nodeLocatorStore.size() << endl << endl;
+  cout << "nodes " << nodeLocatorStore.size() << endl
+       << endl;
 
   numLocs = nodeLocatorStore.size();
 
@@ -170,16 +188,15 @@ void processOSMFile(std::string inputFileName, std::string outDir, OSMSplitConfi
   cout << "Writing out" << endl;
   printMemTimeUpdate();
 
-  OSMSplitWriter osm_writer( 
+  OSMSplitWriter osm_writer(
       config,
       inputFileName,
       outDir,
       nodeLocatorStore,
       options.threadNum,
-      mapHandler.numWays()
-    );
+      mapHandler.numWays());
 }
-void processConfigFile(std::string inputFileName, std::string outDir, OSMSplitConfigPtr& config, SplitOptions options)
+void processConfigFile(std::string inputFileName, std::string outDir, OSMSplitConfigPtr &config, SplitOptions options)
 {
   string inputDir = getDirFromPath(inputFileName);
 
@@ -189,104 +206,115 @@ void processConfigFile(std::string inputFileName, std::string outDir, OSMSplitCo
   file.close();
   rapidjson::Document d;
 
-  if(!d.Parse<0>(ss.str().c_str()).HasParseError() && d.HasMember("osmsplit")) 
-  { 
+  if (!d.Parse<0>(ss.str().c_str()).HasParseError() && d.HasMember("osmsplit"))
+  {
     config = std::make_shared<OSMSplitConfig>(d["osmsplit"]);
-  
+
     OSMConfigList configList = config->getLeafNodes();
-    for(auto leaf : configList) {
+    for (auto leaf : configList)
+    {
 
       string inputFilePath = inputDir + leaf->getFileName();
-      
-      processOSMFile(
-              inputFilePath,
-              outDir, 
-              leaf,
-              options);
 
-      if(options.deleteInputFiles) {
+      processOSMFile(
+          inputFilePath,
+          outDir,
+          leaf,
+          options);
+
+      if (options.deleteInputFiles)
+      {
         fs::remove(inputFilePath);
       }
     }
   }
-  else {
+  else
+  {
     cout << "Faliled to parse config file '" << inputFileName << "'" << endl;
 
     return;
   }
 }
 
-int main(int argi, char** argc) {
+int main(int argi, char **argc)
+{
 
   startTime = std::clock();
 
-  args::ArgumentParser          parser("osmsplit. Take a single large osm file and split it in half N times", 
-    "You must at least specify one input, an output directory, and the number of splits to go down to, ");
+  args::ArgumentParser parser("osmsplit. Take a single large osm file and split it in half N times",
+                              "You must at least specify one input, an output directory, and the number of splits to go down to, ");
 
-  args::ValueFlag<std::string>  inputFileArg(parser, "*.osm|*.pbf", "Specify input .osm file", {'i'});
-  args::ValueFlag<std::string>  outputDirArg(parser, "/", "Specify output directory", {'o'});
-  args::ValueFlag<int>          levelsArg(parser, "1", "Specify number of splits wanted", {'l'});
-  args::ValueFlag<int>          sampleRateArg(parser, "s", "Sample Rate", {'s'});
-  args::ValueFlag<int>          maxThreadsArg(parser, "t", "Max Threads", {'t'});
-  args::Flag                    updateOnlyArg(parser, "u", "Don't redo existing output files if input file is older", {'u'});
-  args::Flag                    deleteInputFilesArg(parser, "d", "Delete input files", {'d'});
+  args::ValueFlag<std::string> inputFileArg(parser, "*.osm|*.pbf", "Specify input .osm file", {'i'});
+  args::ValueFlag<std::string> outputDirArg(parser, "/", "Specify output directory", {'o'});
+  args::ValueFlag<int> levelsArg(parser, "1", "Specify number of splits wanted", {'l'});
+  args::ValueFlag<int> sampleRateArg(parser, "s", "Sample Rate", {'s'});
+  args::ValueFlag<int> maxThreadsArg(parser, "t", "Max Threads", {'t'});
+  args::Flag updateOnlyArg(parser, "u", "Don't redo existing output files if input file is older", {'u'});
+  args::Flag deleteInputFilesArg(parser, "d", "Delete input files", {'d'});
 
   // couldn't get splitwriter to work with normal osm files
   // args::Flag                    outputXMLFormat(parser, "x", "Output to xml format (protobuf is default)", {'x'});
 
   try
   {
-      parser.ParseCLI(argi, argc);
+    parser.ParseCLI(argi, argc);
   }
   catch (args::Help)
   {
-      std::cout << parser;
-      return 1;
+    std::cout << parser;
+    return 1;
   }
   catch (args::ParseError e)
   {
-      std::cerr << e.what() << std::endl;
-      std::cerr << parser;
-      return 1;
+    std::cerr << e.what() << std::endl;
+    std::cerr << parser;
+    return 1;
   }
   catch (args::ValidationError e)
   {
-      std::cerr << e.what() << std::endl;
-      std::cerr << parser;
-      return 1;
+    std::cerr << e.what() << std::endl;
+    std::cerr << parser;
+    return 1;
   }
-  if(!inputFileArg || !outputDirArg || !levelsArg) {
+  if (!inputFileArg || !outputDirArg || !levelsArg)
+  {
 
-      cout << parser;
-      return 1;
+    cout << parser;
+    return 1;
   }
 
   SplitOptions options;
 
-  if(sampleRateArg) {
+  if (sampleRateArg)
+  {
     options.sampleRate = args::get(sampleRateArg);
   }
-  if(levelsArg) {
+  if (levelsArg)
+  {
     options.depthLevels = args::get(levelsArg);
   }
 
-  if(maxThreadsArg) {
+  if (maxThreadsArg)
+  {
     options.threadNum = args::get(maxThreadsArg);
   }
 
-  if(deleteInputFilesArg) {
+  if (deleteInputFilesArg)
+  {
     options.deleteInputFiles = args::get(deleteInputFilesArg);
   }
 
-  if(updateOnlyArg) {
+  if (updateOnlyArg)
+  {
     options.updateOnly = args::get(updateOnlyArg);
   }
 
   // if(outputXMLFormat) {
   //   OSMSplitConfig::setOutputSuffix(".osm");
   // }
-  
-  try {
+
+  try
+  {
 
     string outDir = addSlash(args::get(outputDirArg));
     string inputFileName = args::get(inputFileArg);
@@ -297,60 +325,70 @@ int main(int argi, char** argc) {
 
     int remainingDepthLevels = options.depthLevels;
 
-    while(remainingDepthLevels) {
+    while (remainingDepthLevels)
+    {
 
-      if(remainingDepthLevels > 8) {
+      if (remainingDepthLevels > 8)
+      {
         remainingDepthLevels -= 8;
         options.depthLevels = 8;
       }
-      else {
+      else
+      {
         options.depthLevels = remainingDepthLevels;
         remainingDepthLevels = 0;
       }
-      if(inputFileName.substr(inputFileName.size()-3) == ".js") {
+      if (inputFileName.substr(inputFileName.size() - configFileExt.size()) == configFileExt)
+      {
 
         processConfigFile(inputFileName, outDir, config, options);
 
         configFileName = outDir + fileNameFromPath(inputFileName);
-    
+
         writeConfigFile(configFileName, config);
 
-        if(remainingDepthLevels) {
+        if (remainingDepthLevels)
+        {
           inputFileName = configFileName;
           options.deleteInputFiles = true;
         }
       }
-      else {
+      else
+      {
 
         processOSMFile(inputFileName, outDir, config, options);
 
-        configFileName = outDir + constructOutPrefix(inputFileName) + "_conf.js";
+        configFileName = outDir + constructOutPrefix(inputFileName) + configFileExt;
 
         writeConfigFile(configFileName, config);
 
-        if(options.deleteInputFiles) {
+        if (options.deleteInputFiles)
+        {
           fs::remove(inputFileName);
         }
 
-        if(remainingDepthLevels) {
+        if (remainingDepthLevels)
+        {
           inputFileName = configFileName;
           options.deleteInputFiles = true;
         }
       }
     }
-
-  } catch (std::bad_alloc&) {
+  }
+  catch (std::bad_alloc &)
+  {
 
     osmium::MemoryUsage mem;
-    
-    cout << "Bad alloc: max mem=" << mem.peak() << endl;
 
-  } catch (const std::exception& ex) {
-    
+    cout << "Bad alloc: max mem=" << mem.peak() << endl;
+  }
+  catch (const std::exception &ex)
+  {
+
     cout << "Exception " << ex.what() << endl;
   }
 
   printMemTimeUpdate();
-  
+
   return 0;
 }
