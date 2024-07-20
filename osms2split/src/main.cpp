3f2b8a3fb275a267376fb207fd0a103d0d979c0b
nickblock <nickblock@hotmail.com>
2024-07-20 21:19:13 +0100

    return 0 on succees, file linted

--- osms2split/src/main.cpp
+++ osms2split/src/main.cpp
@@ -1,102 +1,113 @@
 
-#include <iostream>
 #include "args.hxx"
+#include <iostream>
 
-#include <osmium/io/any_input.hpp>
 #include <osmium/handler.hpp>
+#include <osmium/io/any_input.hpp>
 #include <osmium/visitor.hpp>
 
-#include <osmium/index/node_locations_map.hpp>
 #include <osmium/handler/node_locations_for_ways.hpp>
-#include <osmium/index/map/sparse_mem_array.hpp>
 #include <osmium/index/map/flex_mem.hpp>
-#include <osmium/handler/node_locations_for_ways.hpp>
+#include <osmium/index/map/sparse_mem_array.hpp>
+#include <osmium/index/node_locations_map.hpp>
 
 #include "s2splitter.h"
 
-using std::string;
 using std::cout;
 using std::endl;
+using std::string;
 using std::vector;
 
-using NodeLocatorMap = osmium::index::map::SparseMemArray<osmium::unsigned_object_id_type, osmium::Location>;
-using location_handler_type = osmium::handler::NodeLocationsForWays<NodeLocatorMap>;
+using NodeLocatorMap =
+    osmium::index::map::SparseMemArray<osmium::unsigned_object_id_type,
+                                       osmium::Location>;
+using location_handler_type =
+    osmium::handler::NodeLocationsForWays<NodeLocatorMap>;
 
 using GeoUtils::S2Splitter;
 
-vector<string> getKeysOfInterest(string input)
-{
+vector<string> getKeysOfInterest(string input) {
   std::vector<std::string> tokens;
   std::string token;
   std::istringstream tokenStream(input);
   while (std::getline(tokenStream, token, ',')) {
-    if(token.size()) {
+    if (token.size()) {
       tokens.push_back(token);
     }
   }
   return tokens;
 }
-int main(int argi, char* argv[]) {
+int main(int argi, char *argv[]) {
 
-  args::ArgumentParser  parser("osmsplits2. Take a single large osm file and split it into files defined by s2 cells", 
-    "You must at least specify one input osm/som.pbf file, an output directory, and the s2 level to split dowm to");
+  args::ArgumentParser parser(
+      "osmsplits2. Take a single large osm file and split it into files "
+      "defined by s2 cells",
+      "You must at least specify one input osm/som.pbf file, an output "
+      "directory, and the s2 level to split dowm to");
 
-  args::ValueFlag<string>  inputFileArg(parser, "*.osm|*.pbf", "Specify input .osm file", {'i'});
-  args::ValueFlag<string>  outputDirArg(parser, "/", "Specify output directory", {'o'});
-  args::ValueFlag<string>  keysOfInterestArg(parser, "key1,key2", "Comma separated keys to search for. If not set everything is included", {'k'});
-  args::ValueFlag<int>     s2LevelArg(parser, "1", "Specify number of splits wanted", {'l'});
-  args::Flag               outputXmlArg(parser, "x", "Output xml (.osm), default is pbf", {'x'});
+  args::ValueFlag<string> inputFileArg(parser, "*.osm|*.pbf",
+                                       "Specify input .osm file", {'i'});
+  args::ValueFlag<string> outputDirArg(parser, "/", "Specify output directory",
+                                       {'o'});
+  args::ValueFlag<string> keysOfInterestArg(
+      parser, "key1,key2",
+      "Comma separated keys to search for. If not set everything is included",
+      {'k'});
+  args::ValueFlag<int> s2LevelArg(parser, "1",
+                                  "Specify number of splits wanted", {'l'});
+  args::Flag outputXmlArg(parser, "x", "Output xml (.osm), default is pbf",
+                          {'x'});
 
-  try
-  {
-      parser.ParseCLI(argi, argv);
+  try {
+    parser.ParseCLI(argi, argv);
+  } catch (args::Help) {
+    std::cout << parser;
+    std::exit(1);
   }
-  catch (args::Help)
-  {
-      std::cout << parser;
-      std::exit(1);
+
+  if (args::get(inputFileArg).size() == 0 ||
+      args::get(outputDirArg).size() == 0) {
+    std::cout << parser;
+    std::exit(1);
   }
 
-  if(args::get(inputFileArg).size() == 0 || args::get(outputDirArg).size() == 0) 
-  {
-      std::cout << parser;
-      std::exit(1);
-  }
-  
   try {
 
-      NodeLocatorMap nodeLocatorStore;
+    NodeLocatorMap nodeLocatorStore;
 
-      location_handler_type location_handler(nodeLocatorStore);
+    location_handler_type location_handler(nodeLocatorStore);
 
-      int s2Level = args::get(s2LevelArg);
+    int s2Level = args::get(s2LevelArg);
 
-      S2Splitter s2Splitter(s2Level);
+    S2Splitter s2Splitter(s2Level);
 
-      if(args::get(outputDirArg).size() > 0) {
-        s2Splitter.setOutputDirectory(args::get(outputDirArg));
+    if (args::get(outputDirArg).size() > 0) {
+      s2Splitter.setOutputDirectory(args::get(outputDirArg));
+    }
+    if (args::get(outputXmlArg)) {
+      s2Splitter.setOutputXml(true);
+    }
+    if (args::get(keysOfInterestArg).size()) {
+      vector<string> keysOfInterest =
+          getKeysOfInterest(args::get(keysOfInterestArg));
+      for (auto &key : keysOfInterest) {
+        s2Splitter.setKeyOfInterest(key);
       }
-      if(args::get(outputXmlArg)) {
-        s2Splitter.setOutputXml(true);
-      }
-      if(args::get(keysOfInterestArg).size())
-      {
-        vector<string> keysOfInterest = getKeysOfInterest(args::get(keysOfInterestArg));
-        for(auto& key : keysOfInterest) {
-          s2Splitter.setKeyOfInterest(key);
-        }
-      }
+    }
 
-      osmium::io::Reader reader{args::get(inputFileArg), osmium::osm_entity_bits::node | osmium::osm_entity_bits::way};
+    osmium::io::Reader reader{args::get(inputFileArg),
+                              osmium::osm_entity_bits::node |
+                                  osmium::osm_entity_bits::way};
 
-      osmium::apply(reader, location_handler, s2Splitter);
+    osmium::apply(reader, location_handler, s2Splitter);
 
-      cout << "done" << endl;
+    cout << "done" << endl;
 
-  } catch (const std::exception& e) {
-      // All exceptions used by the Osmium library derive from std::exception.
-      std::cerr << e.what() << '\n';
-      std::exit(1);
+  } catch (const std::exception &e) {
+    // All exceptions used by the Osmium library derive from std::exception.
+    std::cerr << e.what() << '\n';
+    std::exit(1);
   }
-}
 
+  return 0;
+}
