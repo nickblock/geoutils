570077a8c1d7f3075fca67841d5c4299c0634923
nickblock <nickblock@hotmail.com>
2024-07-20 21:21:41 +0100

    convert test.py to make use of unittest

--- test.py
+++ test.py
@@ -5,13 +5,15 @@ import create_test_osm_file
 import tempfile
 import re
 import shutil
+import unittest
 
-def run(args):
+
+def runProcess(args):
 
   out = subprocess.run(args, capture_output=True)
 
-  if out.returncode == 0:
-    print(f"Failed to run {args[0]}")
+  if out.returncode != 0:
+    print(f"Failed to run '{" ".join(args)}'")
   
     print(out.stdout)
 
@@ -19,38 +21,50 @@ def run(args):
   
   return True
 
-def setup():
+class GeoUtilsProcesses(unittest.TestCase):
 
-  test_data_dir = os.path.join(tempfile.gettempdir(), "geoutils_test")
-  if(os.path.exists(test_data_dir)):
-    shutil.rmtree(test_data_dir)
-    os.mkdir(test_data_dir)
-  test_file = os.path.join(test_data_dir, "test.osm")
+  def setUp(self):
 
-  print(f"Writing test file {test_file}")
+    self.test_data_dir = os.path.join(tempfile.gettempdir(), "geoutils_test")
+    if(os.path.exists(self.test_data_dir)):
+      shutil.rmtree(self.test_data_dir)
+      os.mkdir(self.test_data_dir)
+    self.test_file = os.path.join(self.test_data_dir, "test.osm")
 
-  create_test_osm_file.execute([51.514853,-0.104486,51.531354,-0.065948], 0.0002, 10.0, test_file)
+    print(f"Writing test file {self.test_file}")
 
-  return [test_data_dir, test_file]
-
-
-if not os.path.exists("osm2assimp"):
-  print("Failed to find osm2assimp, have you built the project and added the 'build' directory to your path?")
-  exit(1)
+    create_test_osm_file.execute([51.514853,-0.104486,51.531354,-0.065948], 0.0002, 10.0, self.test_file)
 
 
-[test_data_dir, test_file] = setup()
+    if not os.path.exists("osm2assimp"):
+      print("Failed to find osm2assimp, have you built the project and added the 'build' directory to your path?")
+      exit(1)
 
-#test split data
-result = run(["osmsplit", "-i", test_file, "-o", test_data_dir, "-s", "1", "-l", "4"])
+  def test_OsmSplit(self):
 
-assert result
+    #test split data
+    result = runProcess(["osmsplit", "-i", self.test_file, "-o", self.test_data_dir, "-s", "1", "-l", "4"])
 
-regex = re.compile('test[0-1]{4}.osm.pbf')
+    self.assertTrue(result)
 
-test_output_files = [f for f in os.listdir(test_data_dir) if re.match(regex, f)]
+    regex = re.compile('test[0-1]{4}.osm.pbf')
 
-assert len(test_output_files) == 16
+    test_output_files = [f for f in os.listdir(self.test_data_dir) if re.match(regex, f)]
+
+    self.assertEqual(len(test_output_files), 16)
+
+  def test_SplitS2Cells(self):
+
+    result = runProcess(["osms2split", "-i", self.test_file, "-o", self.test_data_dir, "-l", "10"])
+
+    self.assertTrue(result)
+
+    self.assertTrue(os.path.exists(os.path.join(self.test_data_dir, "s2_4876030000000000.osm.pbf")))
+    self.assertTrue(os.path.exists(os.path.join(self.test_data_dir, "s2_4876050000000000.osm.pbf")))
+    self.assertTrue(os.path.exists(os.path.join(self.test_data_dir, "s2_48761b0000000000.osm.pbf")))
+    self.assertTrue(os.path.exists(os.path.join(self.test_data_dir, "s2_48761d0000000000.osm.pbf")))
 
 
-
+if __name__ == "__main__":
+    
+    unittest.main()
